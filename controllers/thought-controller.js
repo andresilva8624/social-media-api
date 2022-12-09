4b9636cfc4b40909b201c604ab5c1dec99ddf8f1
andresilva8624@gmail.com <andresilva8624@gmail.com>
2022-12-09 09:25:10 -0800

    updated routes

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -32,7 +32,7 @@ const thoughtController = {
     Thought.create(req.body)
       .then((dbThoughtData) => {
         return User.findOneAndUpdate(
-          { _id: req.body.userId },
+          { username: req.body.username },
           { $push: { thoughts: dbThoughtData._id } },
           { new: true }
         );
